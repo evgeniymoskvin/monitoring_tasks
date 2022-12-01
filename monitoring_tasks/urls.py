0c1e77ccfa0cb4685e750f40d38cef6b3360a15c
Evgeniy <evgeni.moskvin@gmail.com>
2022-12-01 20:05:54 +0300

    add urls

--- monitoring_tasks/urls.py
+++ monitoring_tasks/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ToDo_tasks.urls')),
     path('', include('login.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
