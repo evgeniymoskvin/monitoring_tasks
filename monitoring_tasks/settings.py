de8b0866ac976172d6b522a13cd2983394e308e0
Evgeniy <evgeni.moskvin@gmail.com>
2022-12-10 01:17:27 +0300

    upd urls static

--- monitoring_tasks/settings.py
+++ monitoring_tasks/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATIC_ROOT = 'root/monitoring_tasks/static/'
 
 # Default primary key field type
@@ -133,7 +133,7 @@ LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # MEDIA_ROOT = 'root/monitoring_tasks/media/'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
