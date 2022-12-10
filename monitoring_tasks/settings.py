34b82644c2b914066d4e6d96e2c88fd77a6da49f
Evgeniy <evgeni.moskvin@gmail.com>
2022-12-10 03:48:54 +0300

    upd urls static

--- monitoring_tasks/settings.py
+++ monitoring_tasks/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-# STATIC_ROOT = '/root/monitoring_tasks/static/'
+STATIC_ROOT = '/root/monitoring_tasks/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
