88642b3616679bcd887de67d53c165fd1c7176f0
Evgeniy <evgeni.moskvin@gmail.com>
2023-05-19 01:51:51 +0300

    add new style

--- ToDo_tasks/models.py
+++ ToDo_tasks/models.py
@@ -69,6 +69,7 @@ class Employee(models.Model):
     check_edit = models.BooleanField("Возможность редактирования", default=False)
     can_make_task = models.BooleanField("Возможность выдавать задания", default=True)
     cpe_flag = models.BooleanField("Флаг ГИП (техническая метка)", default=False)
+    mailing_list_check = models.BooleanField("Получать рассылку", default=True)
 
     def __str__(self):
         return f'{self.last_name} {self.first_name} {self.middle_name}'
