a7a0d5b7868944bdbb5c246935d8f143388af833
Evgeniy <evgeni.moskvin@gmail.com>
2023-06-14 07:12:44 +0300

    fix models

--- monitoring_tasks/settings.py
+++ monitoring_tasks/settings.py
@@ -129,6 +129,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+# SESSION_COOKIE_DOMAIN = '.example.com'
+# SESSION_COOKIE_NAME = 'examplesessionid'
+
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'about'
