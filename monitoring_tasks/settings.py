4f7f8abba94b1c58988eb1eaa0ff22955a4bc4d8
Evgeniy <evgeni.moskvin@gmail.com>
2023-07-10 22:38:53 +0300

    upd settings

--- monitoring_tasks/settings.py
+++ monitoring_tasks/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'monitoring_tasks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("SQL_ENGINE", 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'P@ssw0rd'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PRT', '5432')
     }
 }
 
